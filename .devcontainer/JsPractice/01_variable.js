e685f1ae18662a6331fb1e29c7e2ef20273f11cb
Khawaja Moeen <125657999+KhawajaMoeen@users.noreply.github.com>
2024-09-01 19:55:56 +0000

    done

---
+++ .devcontainer/JsPractice/01_variable.js
@@ -0,0 +1,12 @@
+console.log("Moeen");
+
+// VARIABLES
+
+var myName = "Moeen"
+let myFullName = "Khawaja M. Moeen"
+const age = 22
+
+console.log(`My Name is ${myName}, but my full Name is ${myFullName} and I'm ${age} years old.`)
+
+myName = "M.Moeen"
+console.log(myName)
