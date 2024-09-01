b6c99b0c1a0f71c91a48edc248f3e9b8c28f0729
Khawaja Moeen <125657999+KhawajaMoeen@users.noreply.github.com>
2024-09-01 20:40:28 +0000

    done

---
+++ .devcontainer/JsPractice/02_dataTypes.js
@@ -0,0 +1,21 @@
+let myJob = "Software Engineer"
+const myAge = 22
+let isMarried = false
+let disease = null
+let salary;
+
+let myBio =  {
+    fullName : "Khawaja M.Moeen",
+    age : 22,
+    favoriteDish : ["Biryani", "Achar Ghost", "Qourma", "Gulab Jamun"],
+    haveBaby : false,
+    favoriteSport : "Football"
+};
+
+console.log(typeof (`${myJob}`))
+console.log(typeof myAge)
+console.log(typeof isMarried)
+console.log(typeof disease)
+console.log(typeof salary)
+console.log(typeof myBio);
+console.log(typeof myBio.favoriteSport);
