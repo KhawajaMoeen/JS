2d9788e2c1880e4a50d7c72e32cc0322409c0054
Khawaja Moeen <125657999+KhawajaMoeen@users.noreply.github.com>
2024-09-17 11:32:04 +0000

    still uncomplete

--- .devcontainer/JsPractice/06_strings.js
+++ .devcontainer/JsPractice/06_strings.js
@@ -24,6 +24,8 @@ console.log(myName.indexOf("e")); //starting with e and its count is 2 in the st
 
 let myString = new String("Moeenfc");
 console.log(myString);
+console.log(myString.length);
+
 
 // "Anchor", in this method the value u pass as a parameter of anchor is replaced with the attribute ("name") of anchor tag in HTML and this anchor now has value of the given value at JS anchor method. 
 console.log(myString.anchor("Moeen"));
@@ -37,4 +39,35 @@ console.log(myString.at(-8)); // if the parameter that was passed is greater the
 console.log(myString.big()); // this will returns an output of the "Big tag" in which the value of the variable will be placed.
 
 console.log(myString.blink()); // this will also returns an output of the "Blink tag" in which the value of the variable will be placed.
+
+console.log(myString.bold()); // this will also returns an output of the "Bold tag" in which the value of the variable will be placed.
+
+console.log(myString.charAt(0)); // in this we will give a number acc to length of the string as a parameter and this method returns the output of the character that is placed at the given number as a parameter.  
+
+console.log(myString.charCodeAt(1)); // this will give an integer number on the basis of unicode point range
+
+// this will first that unicode as charcodeat and then .tostring that value of codepointat to hexadecimal
+console.log(myString.codePointAt(1));
+console.log(myString.codePointAt(1).toString(16));
+
+console.log(myString.concat(` by ${myName}`)); // concat simply combine value you will give with the string.
+
+let newString = String("ReactJs")
+console.log(`${newString} and type of ${newString} is ${typeof newString}`);
+
+console.log(myString.fixed()); // this will return a tag <tt></tt> with the value of the string which you have used in your JS code.
+
+console.log(myString.fontcolor("red")); // this will return a tag of "font" with the attribute "color" and its value is the parameter you have given in the method.
+
+console.log(myString.fontsize(5)); // this will returns the tag of "font" with the attribute "size" and its value of string is parameter you have given in the method.
+
+console.log(myString.isWellFormed()); // this will pass true or false, it checks the string is in suggorate form or not.
+
+console.log(myString.italics()); // its returns the tag of "italic" with value of variable's value in JS.
+
+console.log(myString.lastIndexOf("f"));
+
+console.log(myString.link("https://mygit.com")); // returns anchor tag with the value of string  in JS and with attribute value of href, the parameter yiu passes in the link method.
+  
+
 
