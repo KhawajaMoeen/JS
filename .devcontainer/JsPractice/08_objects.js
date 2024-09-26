5c9c6d30817c5404c17468d8ccbdb34a3e78ca7a
Khawaja Moeen <125657999+KhawajaMoeen@users.noreply.github.com>
2024-09-26 12:27:27 +0000

    objects

---
+++ .devcontainer/JsPractice/08_objects.js
@@ -0,0 +1,38 @@
+// Objects
+// Methods:
+
+// 1. Singleton Method
+// 2. Object Literals
+
+// Object Literals
+
+
+let myBio = {
+    myName : "Khawaja M. Moeen",
+    myAge : 21,
+    city : "Karachi",
+    studies : "BSSE",
+    contactNumber : "+92312*******",
+    jobPosition : "Teacher",
+    martialStatus : "Single"
+}
+
+// console.log(myBio.myName);
+
+// myBio["myName"] =  "M. Moeen"
+// console.log(myBio["myName"]);
+
+myBio["email"] = "moeen@gmail.com"
+// console.log(myBio);
+
+// Singleton Method to write Object
+
+const userInfo = new Object({
+    userName : "ali",
+    userId : 101234,
+    userEmail : "user@gmail.com",
+    isLoggedIn : true
+}
+)
+// console.log(userInfo["userEmail"]);
+
