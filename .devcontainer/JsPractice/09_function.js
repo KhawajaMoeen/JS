cb0aadd12a6f7819a2a53e8aa761d40676207545
Khawaja Moeen <125657999+KhawajaMoeen@users.noreply.github.com>
2024-10-02 07:14:17 +0000

    function

--- .devcontainer/JsPractice/09_function.js
+++ .devcontainer/JsPractice/09_function.js
@@ -10,14 +10,21 @@
 // addTwoNumbers(4, 5)
 
 2. 
+// function addTwoNumbers(num1 , num2) {
+//     return num1 + num2
+// }
 
-function addTwoNumbers(num1 , num2) {
-    console.log(num1 + num2)
-    // return num1 + num2
+// const result = addTwoNumbers(4, 5)
+// console.log("result : ", result);
+
+function sendMessage(userName) {
+   return `Hello ${userName}, welcome to our website`
 }
 
-const result = addTwoNumbers(4, 5)
+console.log(sendMessage("Moeen"))
 
-// const result = addTwoNumbers(3, 6)
-console.log("result : ", result);
+function shoppingCart(...price) {
+    return price
+}
 
+console.log(shoppingCart(100, 200, 400, 500));
