3e1f579587b6afa9b499c0e66c833ff260be2355
Khawaja Moeen <125657999+KhawajaMoeen@users.noreply.github.com>
2024-10-02 19:27:57 +0000

    to be continued...

--- .devcontainer/JsPractice/09_function.js
+++ .devcontainer/JsPractice/09_function.js
@@ -28,3 +28,32 @@ function shoppingCart(...price) {
 }
 
 console.log(shoppingCart(100, 200, 400, 500));
+
+let user = {
+    userName : "Ali",
+    userId : 1001,
+    accountPrice : 5000
+}
+
+// calling object or keys/values that is coming from unknown user/info
+function callingObject(anyObject) {
+    return `The object, we are calling is: ${Object.keys(anyObject)} ${Object.values(anyObject)}`
+}
+console.log(callingObject(user))
+
+
+//adding an entity to object coming from random data
+let entity ={userEmail : "user@gmail.com"} 
+
+function addingEntityInObject(anyObject) {
+    return Object.assign(anyObject , entity)
+}
+console.log(addingEntityInObject(user))
+
+let fruits = ["Apple", "Mango", "Banana"]
+function callingArray(getArray){
+    let addFruits = getArray.push("dates");
+    return addFruits
+}
+
+console.log(callingArray(fruits))
